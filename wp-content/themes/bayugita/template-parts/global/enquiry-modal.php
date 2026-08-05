e759f2c8f78aad68ba37f50f06f02d508f8c88b5
jayaanggara <jayaanggara112@gmail.com>
2026-08-05 16:15:44 +0800

    update form

--- wp-content/themes/bayugita/template-parts/global/enquiry-modal.php
+++ wp-content/themes/bayugita/template-parts/global/enquiry-modal.php
@@ -11,14 +11,14 @@ $shortcode = bayugita_option( 'enquiry_form_shortcode', '' );
 ?>
 <div id="modal-enquiry" class="modal">
 	<div class="modal__content !max-w-3xl" data-lenis-prevent>
-		<button type="button" data-modal-close class="flex size-9 items-center justify-center border border-gray-200 hover:bg-gray-50">
-			<iconify-icon icon="ph:x"></iconify-icon>
+		<button type="button" data-modal-close class="hover:border-brand hover:text-brand absolute top-6 right-6 flex size-11 cursor-pointer items-center justify-center border border-gray-300 text-dark transition-all">
+			<iconify-icon icon="ph:x" class="text-2xl"></iconify-icon>
 		</button>
 		<div class="text-center">
-			<h2 class="font-playfair !text-2xl md:!text-3xl"><?php esc_html_e( 'Get In Touch With Us', 'bayugita' ); ?></h2>
+			<h2><?php esc_html_e( 'Get In Touch With Us', 'bayugita' ); ?></h2>
 			<p class="mx-auto mt-4 max-w-xl leading-relaxed"><?php esc_html_e( 'We\'d love to hear from you. Whether you\'re planning a visit or have an enquiry about the villa, our team is on hand to assist.', 'bayugita' ); ?></p>
 		</div>
-		<div class="form mt-8">
+		<div class="form mt-10">
 			<?php
 			if ( $shortcode ) {
 				echo do_shortcode( $shortcode ); // phpcs:ignore WordPress.Security.EscapeOutput
